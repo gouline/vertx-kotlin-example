af1ab11ce0ef1a6063c595c6d17d4a391806e5f1
Mike Gouline <mike@gouline.net>
2016-08-03 21:40:18 +1000

    Kotlin talk demo

--- src/main/java/net/gouline/vertxexample/MainVerticle.kt
+++ src/main/java/net/gouline/vertxexample/MainVerticle.kt
@@ -3,13 +3,20 @@ package net.gouline.vertxexample
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.Handler
-import io.vertx.core.http.HttpServerResponse
-import io.vertx.core.json.Json
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 
 @Suppress("unused")
 class MainVerticle : AbstractVerticle() {
+
+    private val MOCK_ISLANDS by lazy {
+        listOf(
+                Island("Kotlin", Country("Russia", "RU")),
+                Island("Stewart Island", Country("New Zealand", "NZ")),
+                Island("Cockatoo Island", Country("Australia", "AU")),
+                Island("Tasmania", Country("Australia", "AU"))
+        )
+    }
 
     override fun start(startFuture: Future<Void>?) {
         val router = createRouter()
@@ -26,49 +33,11 @@ class MainVerticle : AbstractVerticle() {
     }
 
     private fun createRouter() = Router.router(vertx).apply {
-        get("/").handler(handlerRoot)
-        get("/islands").handler(handlerIslands)
-        get("/countries").handler(handlerCountries)
+        get("/test").handler(handlerTest)
     }
 
-    //
-    // Handlers
-
-    val handlerRoot = Handler<RoutingContext> { req ->
-        req.response().end("Welcome!")
+    val handlerTest = Handler<RoutingContext> { req ->
+        req.response().end("Some boring text...")
     }
 
-    val handlerIslands = Handler<RoutingContext> { req ->
-        req.response().endWithJson(MOCK_ISLANDS)
-    }
-
-    val handlerCountries = Handler<RoutingContext> { req ->
-        req.response().endWithJson(
-                MOCK_ISLANDS.map { it.country }.distinct().sortedBy { it.code }
-        )
-    }
-
-    //
-    // Mock data
-
-    private val MOCK_ISLANDS by lazy {
-        listOf(
-                Island("Kotlin", Country("Russia", "RU")),
-                Island("Stewart Island", Country("New Zealand", "NZ")),
-                Island("Cockatoo Island", Country("Australia", "AU")),
-                Island("Tasmania", Country("Australia", "AU"))
-        )
-    }
-
-    //
-    // Utilities
-
-    /**
-     * Extension to the HTTP response to output JSON objects.
-     */
-    fun HttpServerResponse.endWithJson(obj: Any) {
-        this.putHeader(
-                "Content-Type",
-                "application/json; charset=utf-8").end(Json.encodePrettily(obj))
-    }
 }
